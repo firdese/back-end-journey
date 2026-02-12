b8f7fe0d0f2747de551c8d47ce495c2ca9c97728
firdaus-ubuntu <firdausmohdnazri@gmail.com>
2026-02-12 23:20:27 +0800

    add additional properties for Task and TaskGroup model

--- TaskTracker.Domain/Models/Task.cs
+++ TaskTracker.Domain/Models/Task.cs
@@ -7,16 +7,34 @@ public class Task
 {
     [Column("taskid")]
     public int TaskId { get; set; }
-        
+
     [Column("taskdescription")]
-    public string TaskDescription { get; set; }
-        
-    [Column("taskcompleted")]
-    public bool TaskCompleted { get; set; } 
+    public string TaskDescription { get; set; } = string.Empty;
+
+    [Column("taskcreatedatutc")]
+    public DateTime TaskCreatedAtUtc { get; set; }
+
+    [Column("taskupdatedatutc")]
+    public DateTime TaskUpdatedAtUtc { get; set; }
+
+    [Column("taskcompletedatutc")]
+    public DateTime? TaskCompletedAtUtc { get; set; }
+
+    [Column("taskdueatutc")]
+    public DateTime? TaskDueAtUtc { get; set; }
+
+    [Column("taskdeletedatutc")]
+    public DateTime? TaskDeletedAtUtc { get; set; }
+
+    [Column("tasksortorder")]
+    public int TaskSortOrder { get; set; }
+
+    [Column("taskpriority")]
+    public short TaskPriority { get; set; }
 
     [Column("taskgroupid")]
     public int TaskGroupId { get; set; }
-        
+
     [JsonIgnore]
     public TaskGroup? TaskGroup { get; set; }
 }
--- TaskTracker.Domain/Models/TaskGroup.cs
+++ TaskTracker.Domain/Models/TaskGroup.cs
@@ -6,8 +6,24 @@ public class TaskGroup
 {
     [Column("taskgroupid")]
     public int TaskGroupId { get; set; }
-        
+
     [Column("taskgroupdescription")]
-    public string TaskGroupDescription { get; set; }
+    public string TaskGroupDescription { get; set; } = string.Empty;
+
+    [Column("taskgroupcreatedatutc")]
+    public DateTime TaskGroupCreatedAtUtc { get; set; }
+
+    [Column("taskgroupupdatedatutc")]
+    public DateTime TaskGroupUpdatedAtUtc { get; set; }
+
+    [Column("taskgrouparchivedatutc")]
+    public DateTime? TaskGroupArchivedAtUtc { get; set; }
+
+    [Column("taskgroupcolor")]
+    public string? TaskGroupColor { get; set; }
+
+    [Column("taskgroupsortorder")]
+    public int TaskGroupSortOrder { get; set; }
+
     public ICollection<Task>? Tasks { get; set; }
 }
